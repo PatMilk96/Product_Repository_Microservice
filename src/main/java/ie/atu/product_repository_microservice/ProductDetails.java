a2b321cd444c0d6063fa8d8032475c03d91d138d
Patryk Milkiewicz <g00397265@atu.ie>
2023-12-28 19:38:35 +0000

    Added ProductDetails variables

--- src/main/java/ie/atu/product_repository_microservice/ProductDetails.java
+++ src/main/java/ie/atu/product_repository_microservice/ProductDetails.java
@@ -1,4 +1,27 @@
 package ie.atu.product_repository_microservice;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
 public class ProductDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Product name cannot be blank")
+    private String name;
+
+    @NotBlank(message = "price cannot be empty")
+    @Min(0)
+    private Integer price;
+
+    @NotBlank(message = "Product Code cannot be blank")
+    private Long productCode;
 }
