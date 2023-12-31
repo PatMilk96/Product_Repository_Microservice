60c63090a57d6d36ffd7545565be043654a2adf7
Patryk Milkiewicz <g00397265@atu.ie>
2023-12-31 17:31:05 +0000

    updated id in the dummy data

--- src/main/java/ie/atu/product_repository_microservice/DummyData.java
+++ src/main/java/ie/atu/product_repository_microservice/DummyData.java
@@ -16,11 +16,11 @@ public class DummyData implements CommandLineRunner {
      @Override
     public void run(String... args) throws Exception{
          ProductDetails dummy1 = new ProductDetails(1L,  "T-Shirt", 15, 1976756L);
-         ProductDetails dummy2 = new ProductDetails(1L,  "Jeans", 45, 578329L);
-         ProductDetails dummy3 = new ProductDetails(1L,  "Jacket", 60, 209485L);
-         ProductDetails dummy4 = new ProductDetails(1L,  "Shoes", 70, 502947932L);
-         ProductDetails dummy5 = new ProductDetails(1L,  "Glasses", 10, 129372L);
-         ProductDetails dummy6 = new ProductDetails(1L,  "Watch", 25, 885673L);
+         ProductDetails dummy2 = new ProductDetails(2L,  "Jeans", 45, 578329L);
+         ProductDetails dummy3 = new ProductDetails(3L,  "Jacket", 60, 209485L);
+         ProductDetails dummy4 = new ProductDetails(4L,  "Shoes", 70, 502947932L);
+         ProductDetails dummy5 = new ProductDetails(5L,  "Glasses", 10, 129372L);
+         ProductDetails dummy6 = new ProductDetails(6L,  "Watch", 25, 885673L);
          productRepository.save(dummy1);
          productRepository.save(dummy2);
          productRepository.save(dummy3);
