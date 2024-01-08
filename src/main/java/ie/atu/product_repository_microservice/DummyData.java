618216e5f220e32ffbac467ea51bde718e1f3938
Patryk Milkiewicz <g00397265@atu.ie>
2024-01-08 18:24:38 +0000

    changed dummy data to a float

--- src/main/java/ie/atu/product_repository_microservice/DummyData.java
+++ src/main/java/ie/atu/product_repository_microservice/DummyData.java
@@ -14,13 +14,13 @@ public class DummyData implements CommandLineRunner {
      }
 
      @Override
-    public void run(String... args) throws Exception{
-         ProductDetails dummy1 = new ProductDetails(1L,  "T-Shirt", 15, 1976756L);
-         ProductDetails dummy2 = new ProductDetails(2L,  "Jeans", 45, 578329L);
-         ProductDetails dummy3 = new ProductDetails(3L,  "Jacket", 60, 209485L);
-         ProductDetails dummy4 = new ProductDetails(4L,  "Shoes", 70, 502947932L);
-         ProductDetails dummy5 = new ProductDetails(5L,  "Glasses", 10, 129372L);
-         ProductDetails dummy6 = new ProductDetails(6L,  "Watch", 25, 885673L);
+    public void run(String... args) {
+         ProductDetails dummy1 = new ProductDetails(1L,  "T-Shirt", 15.99, 1976756L);
+         ProductDetails dummy2 = new ProductDetails(2L,  "Jeans", 45.99, 578329L);
+         ProductDetails dummy3 = new ProductDetails(3L,  "Jacket", 60.99, 209485L);
+         ProductDetails dummy4 = new ProductDetails(4L,  "Shoes", 70.99, 9502947932L);
+         ProductDetails dummy5 = new ProductDetails(5L,  "Glasses", 10.99, 129372L);
+         ProductDetails dummy6 = new ProductDetails(6L,  "Watch", 25.99, 885673L);
          productRepository.save(dummy1);
          productRepository.save(dummy2);
          productRepository.save(dummy3);
